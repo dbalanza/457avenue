b848f13f8e696489825a1575e1176f3574c7f84e
demy <dbalanza@village88.com>
2019-01-16 11:44:18 +0800

    Added files

--- assets/js/custom/home.js
+++ assets/js/custom/home.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-	console.log('Demy Pogi')
 	$('.parallax-window').parallax();
+	$( document ).scroll(function(e) {
+		( $(window).scrollTop() > $('#splash').height()-65 ) ? $('#splash .navbar').addClass('fixed_style') : $('#splash .navbar').removeClass('fixed_style');
+	});
+	$('body').on('click', '#top_navigation li', scroll_to_div)
+	$('body').on('click', '#footer_links li', scroll_to_div)
 });
+function scroll_to_div(){
+	section_id = $(this).attr('data-section-id');
+	 $('html,body').animate({
+        scrollTop: $("#" + section_id).offset().top
+    }, 'slow');
+}
